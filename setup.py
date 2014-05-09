06b623b22cda5564508dcf93932c08f01c795a64
James Rutherford <james.rutherford@maplecroft.com>
2014-05-09 15:25:08 +0100

    relaxed requirements a bit

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name='deba-bocho',
-    version='0.1.2',
+    version='0.1.3',
     provides=['bocho'],
     description='Slice up PDFs like a pro.',
     long_description=open('README.rst').read(),

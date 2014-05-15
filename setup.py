a13c90c9d5620573438f1c217a6b31adc313d53f
James Rutherford <james.rutherford@maplecroft.com>
2014-05-15 18:24:09 +0100

    version bump for py3k support

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name='deba-bocho',
-    version='0.4.1',
+    version='0.5.0',
     provides=['bocho'],
     description='Slice up PDFs like a pro.',
     long_description=open('README.rst').read(),

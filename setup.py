e3cb07d2b5c406760bbe1637c340cfb70086339d
James Rutherford <james.rutherford@maplecroft.com>
2014-10-15 13:42:31 +0100

    version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name='deba-bocho',
-    version='0.5.2',
+    version='0.5.3',
     provides=['bocho'],
     description='Slice up PDFs like a pro.',
     long_description=open('README.rst').read(),
